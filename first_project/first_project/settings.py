dfb69df6bc2e3bc0cf08bb668ce3886006c16d18
Niha Ahmad <nihaahmad26@gmail.com>
2021-12-07 15:01:01 -0600

    commit change

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a5-k$x%hgk#=$2qf^r2wv%eziqp3qu@2kwq@gmdp6o-rv^&-v6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
